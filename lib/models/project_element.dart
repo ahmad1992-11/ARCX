e62919b42b68886c60ce7174396f8c7f2c5995c4
ahmad1992-11 <a.khazaei1992@gmail.com>
2026-09-15 00:13:15 +0330

    Update project_element.dart

--- lib/models/project_element.dart
+++ lib/models/project_element.dart
@@ -38,4 +38,47 @@ class ProjectElement {
     this.rotation = 0,
     Map<String, dynamic>? properties,
   }) : properties = properties ?? {};
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'projectId': projectId,
+      'name': name,
+      'type': type.name,
+      'x': x,
+      'y': y,
+      'width': width,
+      'height': height,
+      'rotation': rotation,
+      'properties': properties,
+    };
+  }
+
+  factory ProjectElement.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    final typeName =
+        json['type'] as String? ?? 'custom';
+
+    final type = ProjectElementType.values.firstWhere(
+      (item) => item.name == typeName,
+      orElse: () => ProjectElementType.custom,
+    );
+
+    final rawProperties =
+        json['properties'] as Map<String, dynamic>?;
+
+    return ProjectElement(
+      id: json['id'] as String? ?? '',
+      projectId: json['projectId'] as String? ?? '',
+      name: json['name'] as String? ?? '',
+      type: type,
+      x: (json['x'] as num?)?.toDouble() ?? 0,
+      y: (json['y'] as num?)?.toDouble() ?? 0,
+      width: (json['width'] as num?)?.toDouble() ?? 0,
+      height: (json['height'] as num?)?.toDouble() ?? 0,
+      rotation: (json['rotation'] as num?)?.toDouble() ?? 0,
+      properties: rawProperties ?? {},
+    );
+  }
 }
