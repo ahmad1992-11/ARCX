6f0ac95c29a29e6935ae00a3c943cf595597974b
ahmad1992-11 <a.khazaei1992@gmail.com>
2026-09-15 00:12:38 +0330

    Update user_profile.dart

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -16,4 +16,31 @@ class UserProfile {
     this.country = '',
     this.city = '',
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'profession': profession,
+      'country': country,
+      'city': city,
+    };
+  }
+
+  factory UserProfile.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return UserProfile(
+      id: json['id'] as String? ?? '',
+      name: json['name'] as String? ?? '',
+      email: json['email'] as String? ?? '',
+      phone: json['phone'] as String? ?? '',
+      profession:
+          json['profession'] as String? ?? 'Architect',
+      country: json['country'] as String? ?? '',
+      city: json['city'] as String? ?? '',
+    );
+  }
 }
