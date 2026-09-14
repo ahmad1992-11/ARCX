aadd4d9e5207687152ba22feb9563b6d99795b90
ahmad1992-11 <a.khazaei1992@gmail.com>
2026-09-15 00:06:07 +0330

    Update material.dart

--- lib/models/material.dart
+++ lib/models/material.dart
@@ -19,4 +19,30 @@ class MaterialItem {
     this.color = '',
     this.description = '',
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'category': category,
+      'unit': unit,
+      'price': price,
+      'brand': brand,
+      'color': color,
+      'description': description,
+    };
+  }
+
+  factory MaterialItem.fromJson(Map<String, dynamic> json) {
+    return MaterialItem(
+      id: json['id'] as String? ?? '',
+      name: json['name'] as String? ?? '',
+      category: json['category'] as String? ?? '',
+      unit: json['unit'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0,
+      brand: json['brand'] as String? ?? '',
+      color: json['color'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+    );
+  }
 }
