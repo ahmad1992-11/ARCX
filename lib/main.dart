611e0145bb2e8652a8dbe5ad5d18f7526a938094
ahmad1992-11 <a.khazaei1992@gmail.com>
2026-09-15 01:18:23 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'finance_controller.dart';
+import 'material_controller.dart';
+import 'models/finance_transaction.dart';
+import 'models/material.dart';
+import 'models/project.dart';
 import 'services/storage_service.dart';
 
 Future<void> main() async {
