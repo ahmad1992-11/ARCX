246ac5c8a461c0b9a265797f4e785947f2549476
ahmad1992-11 <a.khazaei1992@gmail.com>
2026-09-15 00:04:29 +0330

    Update finance_transaction.dart

--- lib/models/finance_transaction.dart
+++ lib/models/finance_transaction.dart
@@ -25,4 +25,36 @@ class FinanceTransaction {
     DateTime? date,
     this.description = '',
   }) : date = date ?? DateTime.now();
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'income': income,
+      'projectRelated': projectRelated,
+      'projectId': projectId,
+      'date': date.toIso8601String(),
+      'description': description,
+    };
+  }
+
+  factory FinanceTransaction.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return FinanceTransaction(
+      id: json['id'] as String? ?? '',
+      title: json['title'] as String? ?? '',
+      amount: (json['amount'] as num?)?.toDouble() ?? 0,
+      income: json['income'] as bool? ?? false,
+      projectRelated:
+          json['projectRelated'] as bool? ?? false,
+      projectId: json['projectId'] as String?,
+      date: DateTime.tryParse(
+        json['date'] as String? ?? '',
+      ),
+      description:
+          json['description'] as String? ?? '',
+    );
+  }
 }
